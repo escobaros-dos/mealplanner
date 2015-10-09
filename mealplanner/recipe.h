6ee8ff224450b98d912a24ac212dd6b9acb3abff
Raecheal Howard <Howar051@cougar.csusm.edu>
2015-10-08 17:45:17 -0700

    no message

--- mealplanner/recipe.h
+++ mealplanner/recipe.h
@@ -6,10 +6,25 @@ class Recipe
 {
 public:
     Recipe();
+    string name;
+    vector<string> ingredients;
+    string directions;//cooking directions
+    vector<string> findIngredients(string recipeName);
+    // the find ingredients function will use the select from
+
 
 signals:
 
 public slots:
 };
+/*
+class Ingredients
+{
+   string name;
+   int protein;
+   int calories;
+
+
+}*/
 
 #endif // RECIPE_H
