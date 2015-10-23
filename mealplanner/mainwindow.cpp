57b22a2a91e66c873466024c9169c2ca7a8546e3
Raecheal Howard <Howar051@cougar.csusm.edu>
2015-10-22 18:10:08 -0700

    new GUI update <3

--- mealplanner/mainwindow.cpp
+++ mealplanner/mainwindow.cpp
@@ -14,19 +14,52 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::on_pushButton_2_clicked()
-{
-    ui-> stackedWidget -> setCurrentIndex(1);
-    qDebug("index change");//error check
+{   //This should go to the CREATE MEAL PLAN SCREEN
+    //ui-> stackedWidget -> setCurrentIndex(1);
+    //qDebug("index change");//error check
 }
-
-void MainWindow::on_pushButton_3_clicked()
+//-------------------------------------------------------------------
+void MainWindow::on_pushButton_3_clicked()//opens recipe book
 {
     ui -> stackedWidget -> setCurrentIndex(2);
     qDebug("index change");//error check
 }
 
-void MainWindow::on_actionFna_triggered()
+//----------------------------------------Stacked Widget Back Buttons----------------------------------
+void MainWindow::on_pushButton_7_clicked()//StackedWidget(1)
 {
     ui -> stackedWidget -> setCurrentIndex(0);
     qDebug("index change");//error check
+}
+//----------------------------------------------------------
+void MainWindow::on_pushButton_8_clicked()//StackedWidget(2)
+{
+    ui -> stackedWidget -> setCurrentIndex(0);
+    qDebug("index change");//error check
+}
+//--------------------------------------------------------
+
+void MainWindow::on_pushButton_6_clicked()
+{
+    ui -> stackedWidget -> setCurrentIndex(1);
+    qDebug("index change");//error check
+}
+//----------------------------------------------MainWidget Menu Bar
+//--------------------------------------------------------
+void MainWindow::on_actionFna_triggered()// this is the Home button
+{
+    ui -> stackedWidget -> setCurrentIndex(0);
+    qDebug("index change");//error check
+}
+//------------------------------------------------------
+void MainWindow::on_actionRecipe_Book_2_triggered()//menu tab button "Recipe book" opens recipe book
+{
+    ui -> stackedWidget -> setCurrentIndex(2);
+    qDebug("index change");//error check
+}
+//------------------------------------------------------
+void MainWindow::on_actionCreate_Recipe_triggered()//menu tab button "Create recipe" opens CREATE RECIPS
+{
+    //ui -> stackedWidget -> setCurrentIndex(1);
+    //qDebug("index change");//error check
 }
--- mealplanner/mainwindow.h
+++ mealplanner/mainwindow.h
@@ -27,6 +27,16 @@ private slots:
 
     void on_actionFna_triggered();
 
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_actionRecipe_Book_2_triggered();
+
+    void on_actionCreate_Recipe_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
